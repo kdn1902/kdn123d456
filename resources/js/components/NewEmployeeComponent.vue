01d52d509908267fc05c0a981ba576d4dc42a139
kdn1902 <33313068+kdn1902@users.noreply.github.com>
2018-09-17 15:00:38 +0300

    2018-09-17-2

---
+++ resources/js/components/NewEmployeeComponent.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                   			<button type="button" class="btn btn-primary" @click.prevent="createEmployee">Create Employee</button>
+                </div>
+
+                <div class="card-body">
+   				   <template v-if="err">
+						<div v-for="value in err" class="row alert alert-danger" role="alert">
+							{{ value[0] }}
+						</div>
+      			   </template>
+      			   <h1>
+      			   Hello, world!!!	
+      			   </h1>
+      			   
+      			   
+			  </div>
+		  </div>
+      </div>
+   </div>
+</template>
+<script>
+    export default {
+    	data(){
+				return{
+						err:null,
+									
+				}
+			},
+		methods: {
+			createEmployee()
+			{
+				
+			}
+		}
+	}
+</script>
---
+++ resources/views/newemployee.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+	<newemployee-component></newemployee-component>
+</div>
+@endsection
